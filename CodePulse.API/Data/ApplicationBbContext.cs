e84526e5c8eceecb64c5cde057b73b6be98e8b43
ola <olanazmy@gmail.com>
2025-01-24 21:32:38 +0200

    added 2 domain models and DbContext

---
+++ CodePulse.API/Data/ApplicationBbContext.cs
@@ -0,0 +1,18 @@
+﻿using CodePulse.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Data
+{
+    public class ApplicationBbContext : DbContext
+    {
+        public ApplicationBbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //make properities of Models
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
